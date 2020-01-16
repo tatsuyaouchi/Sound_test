076d715c372659b85917f5119c545a8b6e78a50b
tatsuyaouchi <s18b703@shohoku.ac.jp>
2020-01-16 17:28:29 +0900

    まとめたやつ

--- app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Zukan.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Zukan.java
@@ -2,14 +2,20 @@ package jp.ac.shohoku.hguremetal.sound_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Display;
 import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
 
-public class Zukan extends AppCompatActivity {
-
-
+public class Zukan extends AppCompatActivity implements View.OnClickListener{
 
 
     @Override
@@ -17,5 +23,34 @@ public class Zukan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zukan);
 
+        ImageView tapview = findViewById(R.id.imageView1);
+        ImageView tapview2 = findViewById(R.id.imageView2);
+
+        tapview.setOnClickListener(this);
+        tapview2.setOnClickListener(this);
+
+    }
+
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.imageView1:
+                ImageView iv = new ImageView(Zukan.this);
+                iv.setImageResource(R.drawable.no1);
+                iv.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setView(iv)
+                        .show();
+                break;
+            case R.id.imageView2:
+                ImageView iv2 = new ImageView(Zukan.this);
+                iv2.setImageResource(R.drawable.no2);
+                iv2.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setView(iv2)
+                        .show();
+                break;
+        }
+
     }
 }
+
