3567556eb6462c21c3617eb5ad107533f1411ce1
tatsuyaouchi <s18b703@shohoku.ac.jp>
2020-01-20 18:48:15 +0900

    図鑑第一段階

--- app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Zukan.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Zukan.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Display;
@@ -25,9 +26,15 @@ public class Zukan extends AppCompatActivity implements View.OnClickListener{
 
         ImageView tapview = findViewById(R.id.imageView1);
         ImageView tapview2 = findViewById(R.id.imageView2);
+        ImageView tapview3 = findViewById(R.id.imageView3);
+        ImageView tapview4 = findViewById(R.id.imageView4);
+        ImageView tapview5 = findViewById(R.id.imageView5);
 
         tapview.setOnClickListener(this);
         tapview2.setOnClickListener(this);
+        tapview3.setOnClickListener(this);
+        tapview4.setOnClickListener(this);
+        tapview5.setOnClickListener(this);
 
     }
 
@@ -47,6 +54,30 @@ public class Zukan extends AppCompatActivity implements View.OnClickListener{
                 iv2.setAdjustViewBounds(true);
                 new AlertDialog.Builder(Zukan.this)
                         .setView(iv2)
+                        .show();
+                break;
+            case R.id.imageView3:
+                ImageView iv3 = new ImageView(Zukan.this);
+                iv3.setImageResource(R.drawable.no3);
+                iv3.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setView(iv3)
+                        .show();
+                break;
+            case R.id.imageView4:
+                ImageView iv4 = new ImageView(Zukan.this);
+                iv4.setImageResource(R.drawable.no4);
+                iv4.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setView(iv4)
+                        .show();
+                break;
+            case R.id.imageView5:
+                ImageView iv5 = new ImageView(Zukan.this);
+                iv5.setImageResource(R.drawable.no5);
+                iv5.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setView(iv5)
                         .show();
                 break;
         }
