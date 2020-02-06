2df881a67dcbb58b28384cfa64ffdaf35c7ba599
tatsuyaouchi <s18b703@shohoku.ac.jp>
2020-02-06 19:28:58 +0900

    なすくんのやつ

--- app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Summon.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Summon.java
@@ -21,6 +21,8 @@ public class Summon extends AppCompatActivity {
     private FragmentManager fragmentManager;
     // T -> 召喚済み F -> 未召喚
     public boolean fm0, fm1, fm2, fm3, fm4, fm5, fm6, fm7, fm8, fm9 = false;
+    static final int MAX_MONSTER = 10;  // 実装するモンスターの数
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,7 @@ public class Summon extends AppCompatActivity {
         setContentView(R.layout.summon);  // 記述必須：どのレイアウトを使うのか指定
 
         // OCRで読み取った文字列の受取
-        // 文字列を数値に変換する
+        // 文字列を数値(int)に変換する
         Intent intentOcrResult = getIntent();
         ocrResult = Integer.parseInt(intentOcrResult.getStringExtra("EXTRA_OCR_RESULT"));
 
@@ -41,101 +43,20 @@ public class Summon extends AppCompatActivity {
         ImageView iv = findViewById(R.id.image_view);
         AssetManager assets = getResources().getAssets();
 
-        switch(ocrResult) {
-            case 0:
-                fm0 = true;
-                try (InputStream istream0 = assets.open("monster0.png")){
-                    Bitmap bitmap0 = BitmapFactory.decodeStream(istream0);
-                    iv.setImageBitmap(bitmap0);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 1:
-                fm1 = true;
-                try (InputStream istream1 = assets.open("monster1.png")){
-                    Bitmap bitmap1 = BitmapFactory.decodeStream(istream1);
-                    iv.setImageBitmap(bitmap1);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 2:
-                fm2 = true;
-                try (InputStream istream = assets.open("monster2.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 3:
-                fm3 = true;
-                try (InputStream istream = assets.open("monster3.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 4:
-                fm4 = true;
-                try (InputStream istream = assets.open("monster4.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 5:
-                fm5 = true;
-                try (InputStream istream = assets.open("monster5.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 6:
-                fm6 = true;
-                try (InputStream istream = assets.open("monster6.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 7:
-                fm7 = true;
-                try (InputStream istream = assets.open("monster7.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 8:
-                fm8 = true;
-                try (InputStream istream = assets.open("monster8.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            case 9:
-                fm9 = true;
-                try (InputStream istream = assets.open("monster9.png")){
-                    Bitmap bitmap = BitmapFactory.decodeStream(istream);
-                    iv.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
-            default:
-                fragmentManager = getSupportFragmentManager();
-                dialogFragment = new AlertDialogFragment();
-                dialogFragment.show(fragmentManager, "alert dialog");
+        if(0 <= ocrResult && ocrResult < MAX_MONSTER) { // 対応するモンスターの画像表示
+            try (InputStream iStream = assets.open("mon" + ocrResult + ".png")) {
+                Bitmap bitmap = BitmapFactory.decodeStream(iStream);
+                iv.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else { // 正しく読み取れなかった場合は「召喚失敗」を表示
+            try (InputStream iStream = assets.open("failed_summon.png")) {
+                Bitmap bitmap = BitmapFactory.decodeStream(iStream);
+                iv.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
