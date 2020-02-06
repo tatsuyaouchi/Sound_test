aad065364d1a1c352415110df0b3135e7efe6291
usaNasu <usannusann@gmail.com>
2020-02-06 18:47:15 +0900

    Summon.javaのsummonメソッドを簡潔なものに修正した

--- app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Summon.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Summon.java
@@ -26,7 +26,6 @@ public class Summon extends AppCompatActivity {
         ocrResult = Integer.parseInt(intentOcrResult.getStringExtra("EXTRA_OCR_RESULT"));
 
         summon();
-        
     }
 
     // 読み取った数字に応じて召喚するモンスターの決定
