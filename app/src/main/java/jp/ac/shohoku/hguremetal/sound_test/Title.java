0ff7622b0029a901aea7a1767eec4aec57d47239
s18b702 <s18b702@shohhoku.ac.jp>
2020-02-06 17:37:23 +0900

    なんか

--- app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Title.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Title.java
@@ -16,24 +16,17 @@ public class Title extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.title);
         findViewById(R.id.button1).setOnClickListener(this);
 
-        findViewById(R.id.button2).setOnClickListener(this);
+
 
     }
 
 
     public void onClick(View view){
-        switch(view.getId()) {
-            case R.id.button1:
+
+
             Intent intent = new Intent(this, Home.class);
             startActivity(intent);
-            break;
-            case R.id.button2:
-                Intent intent2 = new Intent(this, Asobikata.class);
-                startActivity(intent2);
 
-
-
-        }
     }
 
 
