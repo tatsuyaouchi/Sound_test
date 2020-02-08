138c3723a835854e2d052a9e7d613ddadfde897c
tatsuyaouchi <s18b703@shohoku.ac.jp>
2020-02-08 11:09:34 +0900

    完成

--- app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Summon.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Summon.java
@@ -7,6 +7,7 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,11 @@ import androidx.fragment.app.FragmentManager;
 
 import java.io.InputStream;
 
-public class Summon extends AppCompatActivity {
+public class Summon extends AppCompatActivity implements View.OnClickListener{
     private int ocrResult;
     int ocrResult2;
     static final int MAX_MONSTER = 10;  // 実装するモンスターの数
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,7 @@ public class Summon extends AppCompatActivity {
         setContentView(R.layout.summon);  // 記述必須：どのレイアウトを使うのか指定
 
         findViewById(R.id.homehe).setOnClickListener(this);
+        findViewById(R.id.zukanhe).setOnClickListener(this);
 
 
         // OCRで読み取った文字列の受取
@@ -37,17 +40,14 @@ public class Summon extends AppCompatActivity {
         summon();
 
 
-        // 読み取った文字列をZukanクラスへ送る
-        Intent intentOcrResult2 = new Intent(getApplication(), Zukan.class);
-        intentOcrResult2.putExtra("EXTRA_OCR_RESULT2", ocrResult2);
-//        startActivity(intentOcrResult2);
+
 
     }
 
     // 読み取った数字に応じて召喚するモンスターの決定
     // 対応するモンスターの画像表示
     public void summon(){
-        ImageView iv = findViewById(R.id.image_view);
+        ImageView iv = findViewById(R.id.image_View);
         AssetManager assets = getResources().getAssets();
 
         if(0 <= ocrResult && ocrResult < MAX_MONSTER) { // 対応するモンスターの画像表示
@@ -67,10 +67,22 @@ public class Summon extends AppCompatActivity {
         }
     }
 
+
     public void onClick(View view){
-        Intent intentOcrResult = new Intent(getApplication(), Home.class);
-        intentOcrResult.putExtra("EXTRA_OCR_RESULT2", ocrResult);
-        startActivity(intentOcrResult);
+        switch (view.getId()){
+            case R.id.homehe:
+                Intent intentOcrResult3 = new Intent(getApplication(), Home.class);
+                intentOcrResult3.putExtra("EXTRA_OCR_RESULT3", ocrResult);
+                startActivity(intentOcrResult3);
+                break;
+            case R.id.zukanhe:
+                Intent intentOcrResult2 = new Intent(getApplication(), Zukan.class);
+                startActivity(intentOcrResult2);
+                break;
+        }
+
+
+
 
     }
 }
--- app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Zukan.java
+++ app/src/main/java/jp/ac/shohoku/hguremetal/sound_test/Zukan.java
@@ -22,11 +22,12 @@ import java.io.InputStream;
 
 public class Zukan extends AppCompatActivity implements View.OnClickListener {
 
-    int ocrResult2;
+    int ocrResult;
     int MAX_MONSTER = 10;
     int[] imageviewId = new int[]{
-        R.id.imageView0, R.id.imageView1, R.id.imageView2, R.id.imageView3,
-        R.id.imageView4, R.id.imageView5, R.id.imageView6, R.id.imageView7, R.id.imageView8
+            R.id.imageView0, R.id.imageView1, R.id.imageView2, R.id.imageView3,
+            R.id.imageView4, R.id.imageView5, R.id.imageView6, R.id.imageView7, R.id.imageView8,
+            R.id.imageView9
     };
 
     ImageView[] tapview = new ImageView[10];
@@ -37,103 +38,227 @@ public class Zukan extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zukan);
 
-//         OCRで読み取った文字列の受取
-        Intent intentOcrResult2 = getIntent();
-        ocrResult2 = intentOcrResult2.getIntExtra("EXTRA_OCR_RESULT2", MAX_MONSTER);
-        Zukan();
-
-//        Ocr ocrResult2 = new (Ocr)getIntent().getSerializableExtra("");
-//        String name = ocrResult2.getResult();
-
-        for (int i = 0; i < 9; i++) {
-            tapview[i] = (ImageView)this.findViewById(imageviewId[i]);
+        for (int i = 0; i < 10; i++) {
+            tapview[i] = (ImageView) this.findViewById(imageviewId[i]);
             tapview[i].setOnClickListener(this);
         }
 
-//        // OCRで読み取った文字列の受取
-//        // 文字列を数値(int)に変換する
-//        Intent intentOcrResult2 = getIntent();
-//        ocrRexult2 = Integer.parseInt(intentOcrResult2.getStringExtra("EXTRA_OCR_RESULT"));
+
+//         OCRで読み取った文字列の受取
+//        Intent intentOcrResult = getIntent();
+//        ocrResult = intentOcrResult.getIntExtra("EXTRA_OCR_RESULT2", MAX_MONSTER);
 //        Zukan();
 
-
+//        Ocr ocrResult2 = new (Ocr)getIntent().getSerializableExtra("");
+//        String name = ocrResult2.getResult();
     }
 
-    public void Zukan() {
-////
-//////        if(ocrResult2 != 0) {
-//////            switch (ocrResult2) {
-//////                case 1:
-//////                    ((ImageView) findViewById(R.id.imageView0)).setImageResource(R.drawable.mon0);
-//////                    break;
-//////                case 2:
-//////                    ((ImageView) findViewById(R.id.imageView1)).setImageResource(R.drawable.mon1);
-//////                    break;
-//////                case 3:
-//////                    ((ImageView) findViewById(R.id.imageView2)).setImageResource(R.drawable.mon2);
-//////                    break;
-//////                case 4:
-//////                    ((ImageView) findViewById(R.id.imageView3)).setImageResource(R.drawable.mon3);
-//////                    break;
-//////                case 5:
-//////                    ((ImageView) findViewById(R.id.imageView4)).setImageResource(R.drawable.mon4);
-//////                    break;
-//////                case 6:
-//////                    ((ImageView) findViewById(R.id.imageView5)).setImageResource(R.drawable.mon5);
-//////                    break;
-//////                case 7:
-//////                    ((ImageView) findViewById(R.id.imageView6)).setImageResource(R.drawable.mon6);
-//////                    break;
-//////                case 8:
-//////                    ((ImageView) findViewById(R.id.imageView7)).setImageResource(R.drawable.mon7);
-//////                    break;
-//////                case 9:
-//////                    ((ImageView) findViewById(R.id.imageView8)).setImageResource(R.drawable.mon8);
-//////                    break;
-////////////                case 10:
-////////////                    ((ImageView) findViewById(R.id.imageView9)).setImageResource(R.drawable.mon9);
-////////////                    break;
-//////////
-//////            }
-////        }
-
-
-
-    }
+//
+//    public void Zukan() {
+//
+//        if(ocrResult != 0) {
+//            switch (ocrResult) {
+//                case 0:
+//                    ((ImageView) findViewById(R.id.imageView0)).setImageResource(R.drawable.mon0);
+//                    break;
+//                case 1:
+//                    ((ImageView) findViewById(R.id.imageView1)).setImageResource(R.drawable.mon1);
+//                    break;
+//                case 2:
+//                    ((ImageView) findViewById(R.id.imageView2)).setImageResource(R.drawable.mon2);
+//                    break;
+//                case 3:
+//                    ((ImageView) findViewById(R.id.imageView3)).setImageResource(R.drawable.mon3);
+//                    break;
+//                case 4:
+//                    ((ImageView) findViewById(R.id.imageView4)).setImageResource(R.drawable.mon4);
+//                    break;
+//                case 5:
+//                    ((ImageView) findViewById(R.id.imageView5)).setImageResource(R.drawable.mon5);
+//                    break;
+//                case 6:
+//                    ((ImageView) findViewById(R.id.imageView6)).setImageResource(R.drawable.mon6);
+//                    break;
+//                case 7:
+//                    ((ImageView) findViewById(R.id.imageView7)).setImageResource(R.drawable.mon7);
+//                    break;
+//                case 8:
+//                    ((ImageView) findViewById(R.id.imageView8)).setImageResource(R.drawable.mon8);
+//                    break;
+//                case 9:
+//                    ((ImageView) findViewById(R.id.imageView9)).setImageResource(R.drawable.mon9);
+//                    break;
+//
+//            }
+//        }
+//    }
 
     @Override
     public void onClick(View v) {
+
+
+//        if(ocrResult > 0 && ocrResult < 10){
         switch (v.getId()) {
             case R.id.imageView0:
-            case R.id.imageView6:
+//                    if (ocrResult == 0) {
+                ImageView iv0 = new ImageView(Zukan.this);
+                iv0.setImageResource(R.drawable.mon0);
+                iv0.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("なめろう")
+                        .setMessage("ひとの顔を舐めまわすぞ！！！！")
+                        .setView(iv0)
+                        .show();
+                break;
+//            }
             case R.id.imageView1:
+                ImageView iv1 = new ImageView(Zukan.this);
+                iv1.setImageResource(R.drawable.mon1);
+                iv1.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("こどもどらごん")
+                        .setMessage("成長すると、かっこいいドラゴンになるぞ！！！！")
+                        .setView(iv1)
+                        .show();
+                break;
             case R.id.imageView2:
+                ImageView iv2 = new ImageView(Zukan.this);
+                iv2.setImageResource(R.drawable.mon2);
+                iv2.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("きんぐかまきり")
+                        .setMessage("全長2mもあるぞ！！！！")
+                        .setView(iv2)
+                        .show();
+                break;
             case R.id.imageView3:
+                ImageView iv3 = new ImageView(Zukan.this);
+                iv3.setImageResource(R.drawable.mon3);
+                iv3.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("きらーあっくす")
+                        .setMessage("とても凶暴だぞ！！！！")
+                        .setView(iv3)
+                        .show();
+                break;
             case R.id.imageView4:
-            case R.id.imageView8:
-            case R.id.imageView7:
+                ImageView iv4 = new ImageView(Zukan.this);
+                iv4.setImageResource(R.drawable.mon4);
+                iv4.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("Gorbachev")
+                        .setMessage("ゴルフが趣味だぞ！！！！")
+                        .setView(iv4)
+                        .show();
+                break;
             case R.id.imageView5:
+                ImageView iv5 = new ImageView(Zukan.this);
+                iv5.setImageResource(R.drawable.mon5);
+                iv5.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("らいむ")
+                        .setMessage("手には、人の血がついているぞ！！！！")
+                        .setView(iv5)
+                        .show();
+                break;
+            case R.id.imageView6:
+                ImageView iv6 = new ImageView(Zukan.this);
+                iv6.setImageResource(R.drawable.mon6);
+                iv6.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("ぶっち↑！！！！")
+                        .setMessage("お菓子に間違えられて、食べられてしまうことがあるぞ！！！！")
+                        .setView(iv6)
+                        .show();
+                break;
+            case R.id.imageView7:
+                ImageView iv7 = new ImageView(Zukan.this);
+                iv7.setImageResource(R.drawable.mon7);
+                iv7.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("れっどあい")
+                        .setMessage("しかし目は黒だぞ！！！！")
+                        .setView(iv7)
+                        .show();
+                break;
+            case R.id.imageView8:
+                ImageView iv8 = new ImageView(Zukan.this);
+                iv8.setImageResource(R.drawable.mon8);
+                iv8.setAdjustViewBounds(true);
+                new AlertDialog.Builder(Zukan.this)
+                        .setTitle("")
+                        .setMessage("頭の触覚で催眠術をかけてくるぞ！！！！")
+                        .setView(iv8)
+                        .show();
+                break;
+            case R.id.imageView9:
                 ImageView iv9 = new ImageView(Zukan.this);
-                iv9.setImageResource(R.drawable.hatena);
+                iv9.setImageResource(R.drawable.mon9);
                 iv9.setAdjustViewBounds(true);
                 new AlertDialog.Builder(Zukan.this)
+                        .setTitle("さんふらいおん")
+                        .setMessage("冬になると顔の周りが枯れ落ちるぞ！！！！")
                         .setView(iv9)
                         .show();
                 break;
-            //            case R.id.imageView9:
-//                ImageView iv9 = new ImageView(Zukan.this);
-//                iv9.setImageResource(R.drawable.mon8);
-//                iv9.setAdjustViewBounds(true);
-//                new AlertDialog.Builder(Zukan.this)
-//                        .setView(iv9)
-//                        .show();
-//                break;
-
-
 
         }
+
+
     }
 }
+
+
+
+
+
+//                if(ocrResult != 0) {
+//                    switch (ocrResult) {
+//                        case 0:
+//                            if (ocrResult == 0) {
+//                            ((ImageView) findViewById(R.id.imageView0)).setImageResource(R.drawable.mon0);
+//                            break;}
+//                        case 1:
+//                            if (ocrResult == 1) {
+//                            ((ImageView) findViewById(R.id.imageView1)).setImageResource(R.drawable.mon1);
+//                            break;}
+//                        case 2:
+//                            if (ocrResult == 2) {
+//                            ((ImageView) findViewById(R.id.imageView2)).setImageResource(R.drawable.mon2);
+//                            break;}
+//                        case 3:
+//                            if (ocrResult == 3) {
+//                            ((ImageView) findViewById(R.id.imageView3)).setImageResource(R.drawable.mon3);
+//                            break;}
+//                        case 4:
+//                            if (ocrResult == 4) {
+//                            ((ImageView) findViewById(R.id.imageView4)).setImageResource(R.drawable.mon4);
+//                            break;}
+//                        case 5:
+//                            if (ocrResult == 5) {
+//                            ((ImageView) findViewById(R.id.imageView5)).setImageResource(R.drawable.mon5);
+//                            break;}
+//                        case 6:
+//                            if (ocrResult == 6) {
+//                            ((ImageView) findViewById(R.id.imageView6)).setImageResource(R.drawable.mon6);
+//                            break;}
+//                        case 7:
+//                            if (ocrResult == 7) {
+//                            ((ImageView) findViewById(R.id.imageView7)).setImageResource(R.drawable.mon7);
+//                            break;}
+//                        case 8:
+//                            if (ocrResult == 8) {
+//                            ((ImageView) findViewById(R.id.imageView8)).setImageResource(R.drawable.mon8);
+//                            break;}
+//                        case 9:
+//                            if (ocrResult == 9) {
+//                            ((ImageView) findViewById(R.id.imageView9)).setImageResource(R.drawable.mon9);
+//                            break;}
+//
+//                    }
+//                }
+
+//    }
 
 
 
